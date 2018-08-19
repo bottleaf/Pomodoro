6baf5e78c4a9ee3b3cba3a8f8ccd119a95a2e8d4
bottleaf <andrew.gh.su@gmail.com>
2018-08-18 19:48:38 -0500

    Add button, text, and countdown instances in main activity

--- app/src/main/java/com/andrew/pomodoro/MainActivity.java
+++ app/src/main/java/com/andrew/pomodoro/MainActivity.java
@@ -1,10 +1,15 @@
 package com.andrew.pomodoro;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView countdownText;
+    private Button countdownButton;
+    private CountDownTimer countdownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
