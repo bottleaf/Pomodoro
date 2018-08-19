3276a9c50c21aed58706d74a6da1a3b94620ed41
bottleaf <andrew.gh.su@gmail.com>
2018-08-18 20:36:30 -0500

    Add timer button listener and timer variables

--- app/src/main/java/com/andrew/pomodoro/MainActivity.java
+++ app/src/main/java/com/andrew/pomodoro/MainActivity.java
@@ -3,6 +3,7 @@ package com.andrew.pomodoro;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,9 +11,25 @@ public class MainActivity extends AppCompatActivity {
     private TextView countdownText;
     private Button countdownButton;
     private CountDownTimer countdownTimer;
+    private long timeLeftInMilliseconds = 6000;
+    private boolean timerRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countdownText = findViewById(R.id.countdown_text);
+        countdownButton = findViewById(R.id.countdown_button);
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
+    }
+
+    private void startStop() {
+
     }
 }
