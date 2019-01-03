fa94dc6fece54638ce97d66d1e78276065b28519
bottleaf <andrew.gh.su@gmail.com>
2019-01-03 13:58:05 -0600

    add reset button into java program

--- app/src/main/java/com/andrew/pomodoro/launchBreakActivity.java
+++ app/src/main/java/com/andrew/pomodoro/launchBreakActivity.java
@@ -3,13 +3,21 @@ package com.andrew.pomodoro;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class launchBreakActivity extends AppCompatActivity {
+    private static final long STANDARD_BREAK_LENGTH = (long) 3e5;
+    private static final long TEST_POMODORO_LENGTH = 3000;;
     private Context mContext;
+    private TextView breakTimerText;
+    private Button endBreakButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_break);
         mContext = getApplicationContext();
+        breakTimerText = findViewById(R.id.break_timer_text);
+        endBreakButton = findViewById(R.id.finish_break_button);
     }
 }
