e7d2d70ab5d7b9763c3fbdbbae6e583013705f8b
bottleaf <andrew.gh.su@gmail.com>
2019-01-08 17:28:24 -0600

    Add special button text for ending break early

--- app/src/main/java/com/andrew/pomodoro/launchBreakActivity.java
+++ app/src/main/java/com/andrew/pomodoro/launchBreakActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
 
+import static android.view.View.*;
+
 public class launchBreakActivity extends AppCompatActivity {
     private static final long STANDARD_BREAK_LENGTH = (long) 3e5;
     private static final long TEST_POMODORO_LENGTH = 3000;;
@@ -19,5 +21,7 @@ public class launchBreakActivity extends AppCompatActivity {
         mContext = getApplicationContext();
         breakTimerText = findViewById(R.id.break_timer_text);
         endBreakButton = findViewById(R.id.finish_break_button);
+        endBreakButton.setVisibility(VISIBLE);
+        endBreakButton.setText(getString(R.string.end_break_early));
     }
 }
